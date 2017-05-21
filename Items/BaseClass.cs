8e8df0a29242869d9f1bc340979ee4b55912d5cf
Arif Aman <arifaman59@gmail.com>
2017-05-21 16:11:59 +1000

    Added 2 functions

---
+++ Items/BaseClass.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using System.Configuration;
+using NUnit.Framework;
+using RelevantCodes.ExtentReports;
+using System.Drawing.Imaging;
+using OpenQA.Selenium.Safari;
+using OpenQA.Selenium.Interactions;
+
+namespace Items
+{
+    public class BaseClass
+    {
+
+        public IWebDriver driver = null;
+        public ExtentReports report = ExtentManager.getInstance();
+        public ExtentTest test;
+        public string srcName = null;
+
+        public void OpenBrowser(string browserName)
+        {
+
+            string browser = browserName.ToLower();
+            test.Log(LogStatus.Info, "Selected browser : " + browserName);
+
+            if (browser.Equals("firefox"))
+            {
+                test.Log(LogStatus.Info, "Opening browser");
+                driver = new FirefoxDriver();
+                test.Log(LogStatus.Info, "Browser opened successfully");
+
+            }
+            else if (browser.Equals("chrome"))
+            {
+                test.Log(LogStatus.Info, "Opening browser");
+                driver = new ChromeDriver(FileLocation.DriverDir);
+                test.Log(LogStatus.Info, "Browser opened successfully");
+            }
+            else if (browser.Equals("safari"))
+            {
+                test.Log(LogStatus.Info, "Opening browser");
+                driver = new SafariDriver();
+                test.Log(LogStatus.Info, "Browser opened successfully");
+            }
+            else
+            {
+                ReportFail("Invalid browser");
+            }
+
+            driver.Manage().Window.Maximize();
+            test.Log(LogStatus.Info, "Browser maximized");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            test.Log(LogStatus.Info, "Browser configured successfully");
+
+        }
+
+        public void NavigateTo(string url)
+        {
+            test.Log(LogStatus.Info, "Trying to navigate to :" + url);
+            driver.Url = url;
+            test.Log(LogStatus.Info, "Navigated to " + url + " successfully");
+        }
+
+        public void Type(string locator, string data)
+        {
+            test.Log(LogStatus.Info, "Trying to type " + data + " on " + locator);
+            GetElement(locator).SendKeys(data);
+            test.Log(LogStatus.Info, "Typed successfully");
+        }
+
+        public void PressEnter(string locator)
+        {
+            test.Log(LogStatus.Info, "Trying to press Enter button ");
+            GetElement(locator).SendKeys(Keys.Enter);
+            test.Log(LogStatus.Info, "Pressed Enter button successfully");
+        }
+
+        public void ClickAt(string locator)
+        {
+            test.Log(LogStatus.Info, "Trying to click at " + locator);
+            GetElement(locator).Click();
+            test.Log(LogStatus.Info, "Clicked successfully");
+        }
+
+        public void DoubleClickAt(string locator)
+        {
+            test.Log(LogStatus.Info, "Trying to Double-click at " + locator);
+            Actions act = new Actions(driver);
+            act.DoubleClick(GetElement(locator)).Build().Perform();
+            test.Log(LogStatus.Info, "Double-clicked successfully");
+        }
+
+        
+
+
+
+        public bool IsElementPresent(string locator)
+        {
+
+            IList<IWebElement> ele = null;
+
+            try
+            {
+                test.Log(LogStatus.Info, " Trying to find " + locator + " on the page");
+                ele = driver.FindElements(By.XPath(ConfigurationManager.AppSettings[locator]));
+
+            }
+            catch (Exception e)
+            {
+                ReportFail(e.Message);
+            }
+
+            int totalEle = ele.Count;
+
+            if (totalEle == 0)
+            {
+                return false;
+            }
+
+            else
+            {
+                return true;
+            }
+
+
+        }
+
+
+        public void TakeScreenShotAttachToReport()
+        {
+            test.Log(LogStatus.Info, "Trying to take screen shot");
+            string fileName = DateTime.Now.ToString().Replace(" ", "").Replace("/", "").Replace(":", "") + ".JPG";
+            string dir = FileLocation.ScreenShotDir;
+            srcName = dir + fileName;
+            Screenshot src = ((ITakesScreenshot)driver).GetScreenshot();
+            src.SaveAsFile(srcName, ScreenshotImageFormat.Jpeg);
+            test.Log(LogStatus.Info, "Screen shot taken successfully");
+            test.Log(LogStatus.Info, "Screenshot is :" + srcName);
+            test.Log(LogStatus.Info, "trying to attach screen shot to report");
+            test.Log(LogStatus.Info, "Screenshot is attached :" + test.AddScreenCapture(srcName));
+
+        }
+
+
+        public bool VerifyElementText(string locator, string expectedText)
+        {
+            test.Log(LogStatus.Info, "trying to verify the element text");
+            string eleText = GetElement(locator).Text;
+            if (eleText.Equals(expectedText))
+            {
+                test.Log(LogStatus.Info, "Correct element text");
+                return true;
+            }
+            else
+            {
+                test.Log(LogStatus.Info, "Incorrect element text");
+                return false;
+            }
+        }
+
+        public IWebElement GetElement(string locator)
+        {
+            IWebElement ele = null;
+            try
+            {
+                test.Log(LogStatus.Info, "Trying to find the element :" + locator);
+                ele = driver.FindElement(By.XPath(ConfigurationManager.AppSettings[locator]));
+                test.Log(LogStatus.Info, "Element :" + locator + " is found");
+            }
+            catch (Exception e)
+            {
+                ReportFail(e.Message);
+            }
+            return ele;
+
+        }
+
+      
+        public void ReportPass(string msg)
+        {
+            TakeScreenShotAttachToReport();
+            test.Log(LogStatus.Info, msg);
+            test.Log(LogStatus.Pass, "Test case executed successfully");
+        }
+
+        public void ReportFail(string msg)
+        {
+            TakeScreenShotAttachToReport();
+            test.Log(LogStatus.Info, msg);
+            test.Log(LogStatus.Fail, "Test Execution failed");
+
+        }
+
+        public void ReportIgnore(string msg)
+        {
+            test.Log(LogStatus.Skip, "Test is skipped ");
+        }
+
+        public void CloseBrowser()
+        {
+            driver.Quit();
+        }
+
+    }
+}
---
+++ Items/ToDoClass.cs
@@ -0,0 +1,108 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RelevantCodes.ExtentReports;
+
+namespace Items
+{
+    [TestFixture]
+    public class ToDoClass : BaseClass
+    {
+
+        [Test]
+        public void AddItem()
+        {
+            try
+            {
+                test = report.StartTest("AddItem", "Adding a new item");
+                OpenBrowser("Firefox");
+                NavigateTo("http://todomvc.com");
+                ClickAt("AngularJS_link");
+                System.Threading.Thread.Sleep(2000);
+                Type("ToDoBox", "Aman2");
+                PressEnter("ToDoBox");
+                //verify element has been added in the list
+                ValidateElement("Element1", "Aman2");
+
+
+            }
+            catch (Exception e)
+            {
+                ReportFail("Error while executing the test case : " + e.Message);
+                Assert.Fail(e.Message);
+            }
+        }
+
+
+        [Test]
+        public void EditExistingItem()
+        {
+
+            try
+            {
+                test = report.StartTest("EditExistingItem", "Modifying existing item");
+                OpenBrowser("Chrome");
+                NavigateTo("http://todomvc.com");
+                ClickAt("AngularJS_link");
+                Type("ToDoBox", "Aman3");
+                PressEnter("ToDoBox");
+                //verify element has been added in the list
+                ValidateElement("Element1", "Aman3");
+
+                //edit the content of existing item
+
+                DoubleClickAt("Element1");
+                Type("Element1", "AmanChanged3");
+                PressEnter("Element1");
+                ValidateElement("Element1", "AmanChanged3");
+
+
+            }
+            catch (Exception e)
+            {
+                ReportFail("Error while executing the test case : " + e.Message);
+                Assert.Fail(e.Message);
+            }
+
+
+
+        }
+
+
+        public void ValidateElement(string locator, string locatorText)
+        {
+            if (IsElementPresent(locator))
+            {
+                test.Log(LogStatus.Info, "To do list has an element");
+                if (VerifyElementText(locator, locatorText))
+                {
+                    ReportPass("Element added successfully in the list");
+                }
+                else
+                {
+                    ReportFail("ToDo list is empty");
+                    Assert.Fail("Element not added");
+                }
+            }
+            else
+            {
+                ReportFail("ToDo list is empty");
+                Assert.Fail("Element not added");
+            }
+        }
+
+
+
+        [TearDown]
+        public void CloseAll()
+        {
+            report.EndTest(test);
+            report.Flush();
+            CloseBrowser();
+        }
+
+    }
+}
